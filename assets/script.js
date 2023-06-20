f508aa510258b3fa9035e83477fe746f90048aa9
Smaezz <lea.msi@hotmail.fr>
2023-06-20 15:20:20 +0200

    début JS

--- assets/script.js
+++ assets/script.js
@@ -15,4 +15,37 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
+function slideTo(index) {
+	slides.style.transform = `translateX(-${index * slideWidth}px)`;
+	currentSlide = index;
+  
+	const slideImage = document.querySelector('.slideImage');
+	const bannerText = document.querySelector('.bannerText');
+	slideImage.src = slides[currentSlide].image;
+	bannerText.innerHTML = slides[currentSlide].tagLine;
+  }
+  
+  const banner = document.getElementById('.banner');
+  const arrowLeft = document.querySelector('.arrowLeft');
+  const arrowRight = document.querySelector('.arrowRight');
+  const slideWidth = slides.clientWidth;
+  let currentSlide = 0;
+  
+  arrowLeft.addEventListener('click', () => {
+	if (currentSlide > 0) {
+	  slideTo(currentSlide - 1);
+	}
+  });
+  
+  arrowRight.addEventListener('click', () => {
+	if (currentSlide < slides.length - 1) {
+	  slideTo(currentSlide + 1);
+	}
+  });
+  
+  // Afficher la première diapositive au chargement de la page
+  /*slideTo(0);*/
+
+
